4f7a1d313282d8d774084e36035bbf01fde7be7f
Raghu K <raghuk.vit@gmail.com>
2023-06-26 08:17:19 +0530

    Update tf-module-elasticache/

--- main.tf
+++ main.tf
@@ -3,3 +3,46 @@ resource "aws_elasticache_subnet_group" "main" {
   subnet_ids = var.subnet_ids
   tags       = merge({ Name = "${var.component}-${var.env}" }, var.tags)
 }
+resource "aws_security_group" "main" {
+  name        = "${var.component}-${var.env}-sg"
+  description = "${var.component}-${var.env}-sg"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    from_port   = var.port
+    to_port     = var.port
+    protocol    = "tcp"
+    cidr_blocks = var.sg_subnet_cidr
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.component}-${var.env}-sg"
+  }
+}
+
+
+resource "aws_elasticache_replication_group" "main" {
+  replication_group_id       = "${var.component}-${var.env}"
+  description                = "${var.component}-${var.env}"
+  node_type                  = var.node_type
+  port                       = var.port
+  automatic_failover_enabled = true
+  num_node_groups            = var.num_node_groups
+  replicas_per_node_group    = var.replicas_per_node_group
+  subnet_group_name          = aws_elasticache_subnet_group.main.name
+  parameter_group_name       = "default.redis6.x.cluster.on"
+  security_group_ids         = [aws_security_group.main.id]
+  kms_key_id                 = var.kms_key_arn
+}
+
+
+
+
+
--- variables.tf
+++ variables.tf
@@ -4,3 +4,12 @@ variable "tags" {}
 variable "subnet_ids" {}
 variable "sg_subnet_cidr" {}
 variable "kms_key_arn" {}
+variable "engine" {}
+variable "node_type" {}
+variable "replicas_per_node_group" {}
+variable "num_node_groups" {}
+variable "engine_version" {}
+variable "port" {
+  default = 6379
+}
+variable "vpc_id" {}
