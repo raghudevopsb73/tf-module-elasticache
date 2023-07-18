942efbc1500b06eba22888215422eeed73d98f0c
Raghu K <raghuk.vit@gmail.com>
2023-07-18 08:09:40 +0530

    Update tf-module-elasticache/

--- main.tf
+++ main.tf
@@ -56,6 +56,8 @@ resource "aws_elasticache_cluster" "instance" {
   parameter_group_name = "default.redis6.x"
   engine_version       = var.engine_version
   port                 = 6379
+  security_group_ids   = [aws_security_group.main.id]
+  subnet_group_name    = aws_elasticache_subnet_group.main.name
 }
 
 
